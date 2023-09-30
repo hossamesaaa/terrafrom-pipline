4dabe9c4087328cc59bc2f8a9458e32b87fc1bab
hossamesaaa <hossamesaa2019@gmail.com>
2023-09-30 16:25:35 +0200

    delete old data

--- network-module/IGW.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_internet_gateway" "my_gw" {
-  vpc_id = aws_vpc.general_VPC.id
-  depends_on = [aws_vpc.general_VPC]
-
-  tags = {
-    Name = var.igw_tag
-  }
-}
--- network-module/vpc.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_vpc" "general_VPC" {
-  cidr_block = var.vpc_cidr_block
-   tags = {
-    Name = var.vpc_tag 
-  }
-}
